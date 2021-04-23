9b4b4f9083e8038a2817273193c365983cf171dd
etional <78193568+etional@users.noreply.github.com>
2021-04-22 23:56:06 -0500

    Add most of game process

--- SortGame/js/sort.js
+++ SortGame/js/sort.js
@@ -6,97 +6,96 @@ var wordslist;
 var category1;
 var category2;
 var hintText;
-
-function dragStart(e) {
-  this.style.opacity = '0.4';
-  dragSrcE1 = this;
-  e.dataTransfer.effectAllowed = 'move';
-  e.dataTransfer.setData('text/html', this.innerHTML);
-}
-
-function dragOver(e) {
-  e.preventDefault();
-  e.dataTransfer.dropEffect = 'move';
-  return false;
-}
-
-function dragEnter(e) {
-  this.classList.add('over');
-}
-
-function dragLeave(e) {
-  e.stopPropagation();
-  this.classList.remove('over');
-}
-
-function dragDrop(e) {
-  if (dragSrcE1 != this) {
-    dragSrcE1.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData('text/html');
-  }
-  return false;
-}
-
-function dragEnd(e) {
-  var listitems = document.querySelectorAll('#word .word');
-  [].forEach.call(listItems, function (item) {
-    item.classList.remove('over');
-  });
-  this.style.opacity = '1';
-}
-
-function addEventsDragAndDrop(el) {
-  el.addEventListener('dragstart', dragStart, false);
-  el.addEventListener('dragenter', dragEnter, false);
-  el.addEventListener('dragover', dragOver, false);
-  el.addEventListener('dragleave', dragLeave, false);
-  el.addEventListener('drop', dragDrop, false);
-  el.addEventListener('dragend', dragEnd, false);
-}
+var draggedItem = null;
 
 // create alphabet ul
 var boxes = function () {
 	myWords = document.getElementById("buttons");
 	wordBoxes = document.createElement("ul");
+	wordBoxes.className += "words";
+	wordBoxes.id = "bag";
 
-	for (var i = 0; i < wordslist.length; i++) {
-		wordBoxes.id = "words";
+	for (let i = 0; i < wordslist.length; i++) {
 		list = document.createElement("li");
-		list.id = "word";
+		list.className += "word";
+		list.draggable = true;
 		list.innerHTML = wordslist[i];
 		myWords.appendChild(wordBoxes);
 		wordBoxes.appendChild(list);
 	}
 };
 
+var setDrag = function() {
+  const list_items = document.querySelectorAll(".word");
+  const lists = document.querySelectorAll(".words");
+  
+  for (let i = 0; i < list_items.length; i++) {
+	const item = list_items[i];
+	item.addEventListener('dragstart', function () {
+	  draggedItem = item;
+	  setTimeout(function () {
+		item.getElementsByClassName.display = 'none';
+	  }, 0);
+	});
+
+	item.addEventListener('dragend', function () {
+	  setTimeout(function () {
+		draggedItem.style.display = 'block';
+		draggedItem = null;
+	  }, 0);
+	});
+
+	for (let j = 0; j < lists.length; j ++) {
+	  const list = lists[j];
+	  list.addEventListener('dragover', function (e) {
+		e.preventDefault();
+	  });
+	  list.addEventListener('dragenter', function (e) {
+		e.preventDefault();
+		this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+	  });
+	  list.addEventListener('dragleave', function (e) {
+		if (draggedItem.parentElement != this) {
+		  this.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+		}
+	  })
+	  list.addEventListener('drop', function (e) {
+		draggedItem.parentElement.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+		this.append(draggedItem);
+		this.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+	  });
+	}
+  }
+}
+
 function shuffle(array) {
-	for (var i = array.length - 1; i > 0; i--) {
-		var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
+	for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 	}
 }
 
 // Select Catagory
 var setCategorys = function () {
-	categoryName1.innerHTML = category1;
+  categoryName1.innerHTML = category1;
   categoryName2.innerHTML = category2;
 };
 
 function setWordLists(cat1, cat2) {
-  var array1 = Object.keys(words[cat1]);
-  var array2 = Object.keys(words[cat2]);
+  const array1 = Object.keys(words[cat1]);
+  const array2 = Object.keys(words[cat2]);
   wordslist = array1.concat(array2);
   shuffle(wordslist);
 };
 
 function hintFunc() {
-	var randWord = wordslist[Math.floor(Math.random() * wordslist.length)];
+  const randWord = wordslist[Math.floor(Math.random() * wordslist.length)];
   var cat;
   var h;
-  var found = false;
-  for (var i = 0; i < categories.length; i++) {
+  const found = false;
+  for (let i = 0; i < categories.length; i++) {
     cat = categories[i];
     for (x of Object.keys(words[cat])) {
       if (x == randWord) {
@@ -111,8 +110,59 @@ function hintFunc() {
     }
   }
   hintText = "Category: " + cat + "\n" + "Hint: " + h;
-  swal("", "<div style='font-size :24px;'>" + hintText + "</div>")
+  swal("", "<div style='font-size :24px;'>" + hintText + "</div>");
 };
+
+function answerFunc() {
+  var cat1_list = document.getElementById("category1").getElementsByTagName("li");
+  var cat2_list = document.getElementById("category2").getElementsByTagName("li");
+  var cat1_words = Object.keys(words[category1]);
+  var cat2_words = Object.keys(words[category2]);
+
+  if (document.getElementById("bag").children.length == 0) {
+    var correct = true;
+	for (let i = 0; i < cat1_list.length; i++) {
+	  if (!cat1_words.includes(cat1_list[i].innerHTML)) {
+		correct = false;
+		document.getElementById("bag").append(cat1_list[i]);
+		i--;
+	  }
+	}
+	for (let i = 0; i < cat2_list.length; i++) {
+	  if (!cat2_words.includes(cat2_list[i].innerHTML)) {
+		correct = false;
+		document.getElementById("bag").append(cat2_list[i]);
+		i--;
+	  }
+	}
+	if (correct) {
+	  var success = "Correct!"
+	  swal("", "<div style='font-size :24px;'>" + success + "</div>","success");
+	  reset();
+	  play();
+	}
+	else {
+      var wrong = "Wrong!"
+	  swal("", "<div style='font-size :24px;'>" + wrong + "</div>","error");
+	}
+  }
+  else {
+	var warning = "Not all words are sorted!"
+	swal("", "<div style='font-size :24px;'>" + warning + "</div>","warning");
+  }
+}
+
+function reset() {
+  var cat1 = document.getElementById("category1");
+  var cat2 = document.getElementById("category2");
+  while (cat1.firstChild) {
+	cat1.removeChild(cat1.firstChild);
+  }
+  while (cat2.firstChild) {
+	cat2.removeChild(cat2.firstChild);
+  }
+  document.getElementById("bag").remove();
+}
 
 function play() {
 
@@ -123,31 +173,9 @@ function play() {
   setWordLists(category1, category2);
 
   boxes();
+  setDrag();
   hint.onclick = hintFunc;
-/**
-	if (count % wordslist.length == 0) {
-		shuffle(wordslist)
-		console.log("shuffled");
-	}
-	count += 1;
-	chosenCategory = words[wordslist[count % wordslist.length]][0];
-	word = wordslist[count % wordslist.length];
-	hintText = words[wordslist[count % wordslist.length]][1];
-	setImageSource("assets/" + words[wordslist[count % wordslist.length]][2]);
-
-	word = word.replace(/\s/g, "-");
-	buttons();
-	hint.onclick = hintFunc;
-
-	geusses = [];
-	lives = Math.min(Math.ceil(unique_char(word) * (3 / 2)), 10); // No. of lives will be based on length of the word
-	counter = 0;
-	space = 0;
-	result();
-	comments();
-	selectCat();
-	canvas();
-  */
+  answer.onclick = answerFunc;
 };
 
 window.onload = function () {
@@ -156,52 +184,4 @@ window.onload = function () {
 
 	play();
 
-  /**
-	document.getElementById("level").onclick = function () {
-		swal({
-			title: 'Please Select Difficulty Level',
-			html: "<br>" +
-				'<button id="buttonEasy" class="button btn-lg" style="margin: 0 5px">EASY</button>' +
-				'<button id="buttonMedium" class="button btn-lg" style="margin: 0 5px">MEDIUM</button>' +
-				'<button id="buttonHard" class="button btn-lg" style="margin: 0 5px">HARD</button>',
-			showCancelButton: false,
-			showConfirmButton: false
-		}) //swal
-		document.getElementById("buttonEasy").onclick = function () {
-			words = window.easywords;
-			wordslist = Object.keys(words);
-			correct.parentNode.removeChild(correct);
-			letters.parentNode.removeChild(letters);
-			context.clearRect(0, 0, 400, 400);
-			play();
-			swal.clickConfirm();
-		} //buttonEasy
-		document.getElementById("buttonMedium").onclick = function () {
-			words = window.mediumwords;
-			wordslist = Object.keys(words);
-			correct.parentNode.removeChild(correct);
-			letters.parentNode.removeChild(letters);
-			context.clearRect(0, 0, 400, 400);
-			play();
-			swal.clickConfirm();
-		} //buttonMedium
-		document.getElementById("buttonHard").onclick = function () {
-			words = window.hardwords;
-			wordslist = Object.keys(words);
-			correct.parentNode.removeChild(correct);
-			letters.parentNode.removeChild(letters);
-			context.clearRect(0, 0, 400, 400);
-			play();
-			swal.clickConfirm();
-		}
-	}
-
-	// Reset
-	document.getElementById("reset").onclick = function () {
-		correct.parentNode.removeChild(correct);
-		letters.parentNode.removeChild(letters);
-		context.clearRect(0, 0, 400, 400);
-		play();
-	};
-  */
 };

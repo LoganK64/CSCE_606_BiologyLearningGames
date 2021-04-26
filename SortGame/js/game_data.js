d58820eb0d6354c0c5624d38dba2bf97a2ec6c52
mounikareddykunduru <mounikareddykunduru@tamu.edu>
2021-04-26 15:51:23 -0500

    data

--- SortGame/js/game_data.js
+++ SortGame/js/game_data.js
@@ -1,33 +1,46 @@
 // "category" : {"word" : "Hint"}
 
-//Every word belongs into one of 3 categories: 'Plant', 'Animal or Virus' or 'Unicellular'
+//Every word belongs into one of 3 categories: "Ecosystem organization", "Enerry flow", "Biome"
  
-window.categories = ["Plant", "Animal or Virus", "Unicellular"]
+window.categories = ["Ecosystem organization", "Energy flow", "Biome"]
 
 window.words = {
-"Plant" : {
-    "mitochondria" : "Power house of the cell",
-    "nuclues" : "Brain of a cell",
-    "plant cell" : "Basic unit of life in organisms of the kingdom plantae",
-    "cell wall" : "tough, rigid outer covering that protects and provides shape to the cell",
-    "prokaryotic" : "cells that lack membrane-bound nuclei or organelles",
-    "eukaryotic" : "cells with membrane-bound nuclei and other organelles",
-    "golgi apparatus" : "membrane-bound organelle of flattened stacks where proteins are processed, packaged, and sent out as vesicles",
-    "photosynthesis" : "process in which plants covert solar energy into chemical energy in the form of sugar (glucose)"
+"Ecosystem organization" : {
+    "biome" : "A large region with plants and animals well adapted to the soil and climate of the region.",
+    "biotic" : "All factors related to the living or once living (dead vegetation or animals) parts of the environment.",
+    "ecosystem" : "A biological community of interacting organisms and their physical environment.",
+    "organism" : "Any living thing, a plant, an animal, a bacteria, etc.",
+    "community" : "Interacting populations of various species in a common location.",
+    "biosphere" : "This is the part of Earth that supports life. It includes all biomes,ecosystems, communities, and populations.",
+    "abiotic" : "The nonliving components of an ecosystem.",
+    "biodiversity" : "The variety of living things in a given space.", 
+    "primary-succession" : "Succession that begins in essentially lifeless areas, such as regions in which there is no soil or where the soil is incapable of sustaining life",
+    "secondary-succession": "Succession that occurs in areas where a community existed but has been removed by disturbances that did not eliminate all life and nutrients from the environment",
+    "climax-community": "An ecological community in which populations of plants or animals remain stable and exist in balance with each other and their environment."
 },
-"Animal or Virus" : {
-    "animal cell" : "Basic unit of life in organisms of the kingdom animalia",
-    "virus" : "Infects a host cell",
-    "organ system" : "organs working together to perform certain functions",
-    "rough endoplasmic reticulum" : "membrane-bound organelle studded with ribosomes which modify proteins",
-    "cytoplasm" : "all the contents inside a cell"
+"Energy flow" : {
+    "photosynthesis": "The process through which producers use the sun's energy to make their own food.",
+    "carnivore" : "These animals only eat other animals (meat).",
+    "herbivore" :  "An animal that gets its energy from eating plants alone.",
+    "omnivore" :  "An animal that gets its energy from eating plants and as well as meat.",
+	"food-chain" :  "The model for the transfer of energy from one organism to another in the form of food.", 
+	"energy-pyramid" :  "Compares the energy available at each level of the food chain.",
+	"food-web" : "A series of overlapping food chains that more completely models the flow of energy through an ecosystem.", 
+	"producer" : "Organisms that make their own food.", 
+	"consumer" :  "An organism that cannot produce its own food and must eat other organisms to obtain energy.", 
+	"decomposer" : "An organism that breaks down organic (once living) matter."
+   
 },
-"Unicellular" : {
-    "amoeba" : "Unicellular organism which has the ability to alter its shape",
-    "paramecium" : "Genus of unicellular ciliates, commonly studied as a representative of the ciliate group",
-    "plasma membrane" : "membrane of lipids and proteins that forms the external boundary of a cell and serves as a gate keeper letting only select things in and out",
-    "cytosol" : "gel-like fluid that fills the cell, holds organelles in place, and aids in waste break down and energy transformation (metabolism)",
-    "ribosomes" : "Non-membrane bound structures which make proteins",
-    "smooth endoplasmic reticulum" : "membrane-bound organelle not studded with ribosomes which modify proteins"
+"Biome" : {
+    "tundra" :  "Cold desert. Permafrost is the layer of permanently frozen soil characteristic of this biome.", 
+	"taiga" : "The world's largest biome is a cold forest. Precipitation averages 60cm a year and the average temperature is 0 degree Celsius.", 
+	"temperate-deciduous-forest" :  "These biomes are known for their four distinct seasons.", 
+	"rainforest" :  "These biomes are are defined by the significant amount of rainfall they receive. There are two types, temperate and tropical.", 
+	"desert" :  "The driest biome on earth.", 
+	"grassland" :  "Biome where the vegetation is dominated by grasses.", 
+	"aquatic" :  "Biome habitats that are dominated by water.",
+	"freshwater" : "These ecosystems include flowing water such as rivers and creeks and standing water such as lakes, ponds, and wetlands.", 
+	"marine" :  "These ecosystems are areas of water containing a high salt content."
+
 }
 }
